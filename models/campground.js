d4b9d9c03b798c2b966d2b33ab50090bfb458383
baotruong04 <112520988+baotruong04@users.noreply.github.com>
2024-08-01 17:35:29 -0700

    Add files via upload

---
+++ models/campground.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+    console.log('DELETE');
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
